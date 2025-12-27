1181fdf9527d976aecde01e80c7852da14bdcf0b
admclamb <adylanmclamb@yahoo.com>
2025-12-27 05:23:32 +0000

    Setting up di for application core and di

--- src/ApplicationCore/DependencyInjection.cs
+++ src/ApplicationCore/DependencyInjection.cs
@@ -1,6 +1,18 @@
+using System.Reflection;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace ApplicationCore;
 
-public class DependencyInjection
+public static class DependencyInjection
 {
-    
+    public static IServiceCollection AddApplicationCore(this IServiceCollection services)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly((assembly)));
+        services.AddValidatorsFromAssembly(assembly);
+
+        return services;
+    }
 }
--- src/PublicApi/Program.cs
+++ src/PublicApi/Program.cs
@@ -1,8 +1,11 @@
+using ApplicationCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddApplicationCore();
 builder.Services.AddControllers();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
