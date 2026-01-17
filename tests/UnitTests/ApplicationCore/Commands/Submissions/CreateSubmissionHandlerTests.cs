5696e5b33b0862aed024f620177b2ca7ccef1820
admclamb <adylanmclamb@yahoo.com>
2026-01-17 14:30:01 -0500

    Add tests and test sonar scan

--- tests/UnitTests/ApplicationCore/Commands/Submissions/CreateSubmissionHandlerTests.cs
+++ tests/UnitTests/ApplicationCore/Commands/Submissions/CreateSubmissionHandlerTests.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
-using ApplicationCore.Commands.Submissions.CreateSubmission;
+﻿using ApplicationCore.Commands.Submissions.CreateSubmission;
 using ApplicationCore.Domain.Submissions;
 using ApplicationCore.Interfaces.Repositories;
 using FluentValidation;
