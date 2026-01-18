2ecbd71b12a8398d7bd0e0addaa08b61c2ebff32
admclamb <adylanmclamb@yahoo.com>
2026-01-18 01:45:47 -0500

    Setup entities

---
+++ src/Infrastructure/Persistence/Entities/Submission/Outbox/SubmissionOutboxEntity.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Infrastructure.Persistence.Entities.Submission.Outbox;
+
+[Table("submission_outbox")]
+public sealed class SubmissionOutboxEntity
+{
+    [Key, Column("id")]
+    public Guid Id { get; set; }
+
+    [Required, Column("submission_id")]
+    public required Guid SubmissionId { get; set; }
+
+    [ForeignKey(nameof(SubmissionId))]
+    public SubmissionEntity? Submission { get; set; }
+
+    [Required, Column("submission_outbox_type_id")]
+    public required int SubmissionOutboxTypeId { get; set; }
+
+    [ForeignKey(nameof(SubmissionOutboxTypeId))]
+    public SubmissionOutboxTypeEntity? SubmissionOutboxType { get; set; }
+
+    [Required, Column("submission_outbox_status_id")]
+    public required int SubmissionOutboxStatusId { get; set; }
+
+    [ForeignKey(nameof(SubmissionOutboxStatusId))]
+    public SubmissionOutboxStatusEntity? SubmissionOutboxStatus { get; set; }
+
+    [Column("attempt_count")]
+    public int AttemptCount { get; set; }
+
+    [Column("next_attempt_on")]
+    public DateTime? NextAttemptOn { get; set; }
+
+    [Column("last_error")]
+    public string? LastError { get; set; }
+
+    [Column("created_on")]
+    public DateTime CreatedOn { get; set; }
+
+    [Column("process_on")]
+    public DateTime ProcessOn { get; set; }
+}
