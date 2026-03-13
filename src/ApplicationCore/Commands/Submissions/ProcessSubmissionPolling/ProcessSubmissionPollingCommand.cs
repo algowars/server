3332534167d73147aff8d42cb5f66d7fed02bbd6
admclamb <adylanmclamb@yahoo.com>
2026-03-12 23:19:56 -0400

    fix

--- src/ApplicationCore/Commands/Submissions/ProcessSubmissionPolling/ProcessSubmissionPollingCommand.cs
+++
@@ -1,7 +0,0 @@
-using ApplicationCore.Domain.Submissions;
-using MediatR;
-
-namespace ApplicationCore.Commands.Submissions.ProcessSubmissionPolling;
-
-public sealed record ProcessSubmissionPollingCommand(IEnumerable<SubmissionModel> Submissions)
-    : ICommand<Unit>;
