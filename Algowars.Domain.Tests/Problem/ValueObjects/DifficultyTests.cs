1e0d95c65ab9e2c30a6022b2ddb4b9ba84eab473
admclamb <adylanmclamb@yahoo.com>
2026-06-11 23:22:52 -0400

    add difficulty test

---
+++ Algowars.Domain.Tests/Problem/ValueObjects/DifficultyTests.cs
@@ -0,0 +1,63 @@
+using Algowars.Domain.Problem.Enums;
+using Algowars.Domain.Problem.Exceptions;
+using Algowars.Domain.Problem.ValueObjects;
+
+namespace Algowars.Domain.Tests.Problem.ValueObjects;
+
+public class DifficultyTests
+{
+    [Test]
+    public void Constructor_AtMinValue_Succeeds()
+    {
+        var difficulty = new Difficulty(Difficulty.MinValue);
+
+        Assert.That(difficulty.Value, Is.EqualTo(Difficulty.MinValue));
+    }
+
+    [Test]
+    public void Constructor_BelowMinValue_ThrowsInvalidDifficultyException()
+    {
+        Assert.Throws<InvalidDifficultyException>(() => new Difficulty(Difficulty.MinValue - 1));
+    }
+
+    [Test]
+    public void Equality_DifferentValues_AreNotEqual()
+    {
+        var a = new Difficulty(100);
+        var b = new Difficulty(200);
+
+        Assert.That(a, Is.Not.EqualTo(b));
+    }
+
+    [Test]
+    public void Equality_SameValue_AreEqual()
+    {
+        var a = new Difficulty(500);
+        var b = new Difficulty(500);
+
+        Assert.That(a, Is.EqualTo(b));
+    }
+
+    [TestCase(0, DifficultyTier.Easy)]
+    [TestCase(500, DifficultyTier.Easy)]
+    [TestCase(1000, DifficultyTier.Easy)]
+    [TestCase(1001, DifficultyTier.Medium)]
+    [TestCase(1500, DifficultyTier.Medium)]
+    [TestCase(2000, DifficultyTier.Medium)]
+    [TestCase(2001, DifficultyTier.Hard)]
+    [TestCase(3000, DifficultyTier.Hard)]
+    public void Tier_ReturnsCorrectTierForValue(int value, DifficultyTier expectedTier)
+    {
+        var difficulty = new Difficulty(value);
+
+        Assert.That(difficulty.Tier, Is.EqualTo(expectedTier));
+    }
+
+    [Test]
+    public void ToString_IncludesValueAndTier()
+    {
+        var difficulty = new Difficulty(500);
+
+        Assert.That(difficulty.ToString(), Does.Contain("500").And.Contain("Easy"));
+    }
+}
