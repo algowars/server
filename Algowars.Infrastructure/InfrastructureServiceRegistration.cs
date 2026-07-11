da3c33f950c411085b67f3a4261fc5435293e489
admclamb <adylanmclamb@yahoo.com>
2026-07-11 17:50:31 -0400

    Add infra seeding

--- Algowars.Infrastructure/InfrastructureServiceRegistration.cs
+++ Algowars.Infrastructure/InfrastructureServiceRegistration.cs
@@ -50,7 +50,7 @@ public static class InfrastructureServiceRegistration
         this IServiceCollection services,
         IConfiguration configuration)
     {
-        services.AddOptions(configuration);
+        services.AddOption<ConnectionStringOptions>(configuration);
         services.AddPersistence();
         services.AddSeeder();
 
